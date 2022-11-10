7e0165b8790e3944bf0cdf1cfc64502e22c7000b
smartvain <zlc.blue15210@gmail.com>
2022-11-10 15:35:14 +0900

    attach: TabsWrapper style

--- src/components/TabsWrapper.vue
+++ src/components/TabsWrapper.vue
@@ -14,6 +14,7 @@ provide('selectedTitle', selectedTitle)
       <li
         v-for="title in tabTitles"
         :key="title"
+        :class="{ selected: title == selectedTitle }"
         @click="selectedTitle = title"
       >
         {{ title }}
@@ -23,5 +24,33 @@ provide('selectedTitle', selectedTitle)
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
+.tabs {
+  max-width: 480px;
+  margin: 0 auto;
+}
+
+.tabs-header {
+  margin-bottom: 10px;
+  list-style: none;
+  padding: 0;
+  display: flex;
+}
+
+.tabs-header li {
+  width: 80px;
+  text-align: center;
+  padding: 10px 20px;
+  margin-right: 10px;
+  background-color: #ddd;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.4s all ease;
+  color: darken($color: #000, $amount: 0);
+}
+
+.tabs-header li.selected {
+  background-color: #0984e3;
+  color: white;
+}
 </style>
