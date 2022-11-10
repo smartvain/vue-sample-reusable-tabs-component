4e12d706b573e36e3f1c95dd39e117a0cc266429
smartvain <zlc.blue15210@gmail.com>
2022-11-10 14:55:58 +0900

    add: Tab component and TabsWrapper component

---
+++ src/components/TabsWrapper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, useSlots } from 'vue'
+
+const slots = useSlots()
+console.log(slots.default().map(tab => tab.props.title))
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style>
+</style>
